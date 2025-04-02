e44d6cec567b3660394d2c690dff9f3c01ad2702
Omar Shahin <os.omar.shahin@gmail.com>
2025-04-02 01:25:58 -0700

    Temp Dialogue Fisherman
    Temp Dialogue Fisherman

--- Omar/Dialogue/DialogueNPC.gd
+++ Omar/Dialogue/DialogueNPC.gd
@@ -2,31 +2,45 @@ extends CanvasLayer
 
 @export_file ("*.json") var d_file
 
+var dialogue = []
+var current_dialogue_id = 0
+var d_active = false
+
 func _ready() -> void:
+	$NinePatchRect.visable = false
 	start()
-#func start():
-#	dialogue = load_dialogue()
-#	$NinePatchRect/Name.text = dialogue[0]['name']
-#	$NinePatchRect/Chat.text = dialogue[0]['text']
+
+func start():
+	if d_active:
+		return
+	d_active = true
+	$NinePatchRect.visable = true
+	dialogue = load_dialogue()
+	current_dialogue_id = -1
+	next_script()
 
 func load_dialogue():
 	var file = FileAccess.open ("res://Omar/Dialogue/json/", FileAccess.READ)
 	var content = JSON.parse_string(file.get_as_text())
 	return content
 
-var dialogue = []
+func _input(event):
+	if not d_active:
+		return
+	if event.is_action_pressed("ui_accept"):
+		next_script()
 
-#func load_dialogue():
-#	if d_file.is_empty():
-#		print ("error: no file selected")
-#		return []
+func next_script():
+	current_dialogue_id += 1
+	
+	if current_dialogue_id >= len(dialogue):
+		$Timer.start()
+		$NinePatchRect.visable = false
+		return
+	$NinePatchRect/Name.text = dialogue[current_dialogue_id]['name']
+	$NinePatchRect/Chat.text = dialogue[current_dialogue_id]['text']
 
-func start():
-	dialogue = load_dialogue()
-	print("Loaded dialogue:", dialogue)  # Debugging output
 
-	if dialogue.size() > 0:
-		$NinePatchRect/Name.text = dialogue[0]['name']
-		$NinePatchRect/Chat.text = dialogue[0]['text']
-	else:
-		print("Error: Dialogue file is empty or not formatted correctly.")
+
+func _on_timer_timeout() -> void:
+	d_active = false
