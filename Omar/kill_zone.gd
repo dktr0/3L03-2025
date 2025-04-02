1426da3d7662d520e47ea5edb3e314a7272b1dd3
Omar Shahin <os.omar.shahin@gmail.com>
2025-04-01 23:36:17 -0700

    Present Updates
    Present Updates

---
+++ Omar/kill_zone.gd
@@ -0,0 +1,9 @@
+extends Area3D
+
+
+func _on_body_entered(body: Node3D) -> void:
+	if body is CharacterBody3D:
+		call_deferred ("reload_scene")
+
+func reload_scene() -> void:
+		get_tree().reload_current_scene()
